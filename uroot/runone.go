9932d85f98caf5d2fa4e5dd1a24d3c7bc0d31071
Ron Minnich <rminnich@gmail.com>
2023-11-15 13:12:21 -0700

    first uroot command runs
    still have a problem with exit, that's the next thing to fix.
    
    Signed-off-by: Ron Minnich <rminnich@gmail.com>

--- uroot/runone.go
+++ uroot/runone.go
@@ -1,14 +1,14 @@
 package bbtamago
 
 import bbmain "bb.u-root.com/bb/pkg/bbmain"
-"log"
+import "log"
 import "os"
 import "flag"
+import "strings"
 
-func runone(c *Command) error {
-	// put a recover here for the panic at some point.
-   os.Args = append([]string{c.cmd}, c.argv...)
-   flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.PanicOnError)
-   log.Printf("run %v", c)
-   return bbmain.Run(c.cmd)
+func runone(s string) error {
+	os.Args = strings.Split(s, " ")
+	log.Printf("run %v", os.Args)
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.PanicOnError)
+	return bbmain.Run(os.Args[0])
 }
